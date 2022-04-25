5620c36dc971b3eafc3a0f48fb76c838cc24be3d
dimitrilavin <dlavin@live.unc.edu>
2022-04-24 21:16:47 -0400

    guess-flip

---
+++ guess-flip.js
@@ -0,0 +1,15 @@
+import { flipACoin } from "./modules/coin.mjs"
+import minimist from "minimist"
+
+const args = minimist(process.argv.slice(2))
+args["call"]
+const call = args.call
+var flip = flipACoin(call)
+
+if (call == null){
+    console.log("Error: no input")
+} else if(call != "heads" && call != "tails") {
+    console.log('Usage: node guess-flip --call=[heads|tails]')
+} else {
+    console.log(flip)
+}
