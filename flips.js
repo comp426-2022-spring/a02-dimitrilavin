d4d1a6048748e36609e33f895ef0ec69011fcb27
dimitrilavin <dlavin@live.unc.edu>
2022-04-24 21:22:02 -0400

    module error fix

--- flips.js
+++ flips.js
@@ -1,6 +1,6 @@
-import { coinFlips } from "./modules/coin.mjs";
-import minimist from "minimist";
-import { countFlips } from "./modules/coin.mjs";
+import { coinFlips } from "./modules/coin.mjs"
+import minimist from "minimist"
+import { countFlips } from "./modules/coin.mjs"
 
 const args = minimist(process.argv.slice(2))
 args["number"]
